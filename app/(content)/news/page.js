5aeab19223b1cbf1baf81ea2277c78c1d159577d
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-08 20:39:00 +0530

    :zap: :art: :calling: Client Side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,50 @@
+'use client';
 import NewsLink from "@/components/news-link";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('')
+  const [news, setNews] = useState()
+
+
+  useEffect(() => {
+    async function fetchNews() {
+      setLoading(true)
+      const response = await fetch('http://localhost:8080/news')
+
+      if (!response.ok) {
+        setError('Failed to fetch news.')
+        setLoading(false)
+      }
+
+      const data = await response.json();
+      setNews(data)
+      setLoading(false)
+    }
+    fetchNews()
+  }, [])
+
+
+  if (loading) {
+    return <p>Loading....</p>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsLink news={news} />
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsLink news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
