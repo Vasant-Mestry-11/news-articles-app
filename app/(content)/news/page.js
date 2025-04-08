a62cf9c7d44078677d0d36844dde0a5d1186d961
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-08 21:05:33 +0530

    :zap: :art: :cloud: Data Fetching directly from backend

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,50 +1,17 @@
-'use client';
 import NewsLink from "@/components/news-link";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const response = await fetch('http://localhost:8080/news')
 
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('')
-  const [news, setNews] = useState()
-
-
-  useEffect(() => {
-    async function fetchNews() {
-      setLoading(true)
-      const response = await fetch('http://localhost:8080/news')
-
-      if (!response.ok) {
-        setError('Failed to fetch news.')
-        setLoading(false)
-      }
-
-      const data = await response.json();
-      setNews(data)
-      setLoading(false)
-    }
-    fetchNews()
-  }, [])
-
-
-  if (loading) {
-    return <p>Loading....</p>
+  if (!response.ok) {
+    throw new Error('Failed to fetch news')
   }
-
-  if (error) {
-    return <p>{error}</p>
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsLink news={news} />
-  }
+  const news = await response.json(); // JSX wont get executed till we receive data
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsLink news={news} />
     </>
   );
 }
